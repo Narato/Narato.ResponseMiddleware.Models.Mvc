3283563ff3c0757011c0a5e6083e8b095393082b
Nicolaas Van Bladel <nicolaas.vanbladel@narato.be>
2018-06-27 13:32:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Narato.ResponseMiddleware.Models.Mvc
+This library contains the core Mvc models for use in the narato stack
+
+# Helping out
+
+If you want to help out, please read [this wiki page](https://github.com/Narato/Narato.ResponseMiddleware.Models/wiki/Helping-out)
---
+++ src/Narato.ResponseMiddleware.Models.Mvc/ActionResults/InternalServerObjectResult.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Narato.ResponseMiddleware.Models.Mvc.ActionResults
+{
+    public class InternalServerObjectResult : ObjectResult
+    {
+        public InternalServerObjectResult(object value) : base(value)
+        {
+            StatusCode = StatusCodes.Status500InternalServerError;
+        }
+    }
+}
